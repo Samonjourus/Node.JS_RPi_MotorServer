7023fb436de3534b1c5b7697f71b810ae98620ab
benjamin standfield <benjaminstandfield@gmail.com>
2018-11-08 13:09:31 -0500

    made basic angualar webpage. needs testing on Pi as well as services

--- homepage/src/app/app.component.ts
+++ homepage/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'homepage';
+  motor1PWMModifier=0;
+  motor2PWMModifier=0;
+  motor3PWMModifier=0;
+
+  constructor(private http: HttpClient){
+    this.getStatus().subscribe((data)=>{
+      this.motor1PWMModifier = 1;
+      this.motor2PWMModifier = 2;
+      this.motor3PWMModifier = 3;
+    })
+  }
+
+  getStatus(){
+    return this.http.get('/status')
+  }
+
 }
